d007b2df5a970cc50c2d788d0daf3f42b402d716
paulo <paulomax9002@gmail.com>
2019-02-17 18:03:11 -0300

    commit

--- app.js
+++ app.js
@@ -113,6 +113,12 @@ function processCommand(recievedMessage) {
 			file: "https://i.imgur.com/uOypeJW.jpg"
 		})
 	}
+
+	if(primaryCommand === "rola") {
+		recievedMessage.channel.send({
+			file: "https://img.buzzfeed.com/buzzfeed-static/static/2018-05/7/16/enhanced/buzzfeed-prod-web-02/enhanced-14853-1525726352-1.jpg"
+		})
+	}
 	
 	if(primaryCommand === "lyrics") {
 
